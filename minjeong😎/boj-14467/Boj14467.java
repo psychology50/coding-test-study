c8bf92466570a9441ff77cd9e42219a351a6789b
byeon22 <minjeongs0202@gmail.com>
2023-09-15 14:51:10 +0900

    fix: 코드리뷰 후 boj-14467, 20546 수정

--- minjeong😎/boj-14467/Boj14467.java
+++ minjeong😎/boj-14467/Boj14467.java
@@ -1,31 +1,82 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class BOJ14467 {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int observation = in.nextInt();
-				
-		int[] cows = new int[10];
-		boolean[] mark = new boolean[10]; // false∑Œ √ ±‚»≠ µ 
+public class BOJ20546 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int cnt = 0;
+		int bnpBalance = Integer.parseInt(br.readLine());
+		int timingBalance = bnpBalance;
+
+		int[] cost = new int[14];
+
+		for (int i = 0; i < 14; i++) {
+			cost[i] = Integer.parseInt(br.readLine());
+		}
+
+		int bnpResult = BNP(bnpBalance, cost);
+		int timingResult = Timing(timingBalance, cost);
+
+		if (bnpResult > timingResult) {
+			System.out.println("BNP");
+		} else if (bnpResult < timingResult) {
+			System.out.println("TIMING");
+		} else {
+			System.out.println("SAMESAME");
+		}
+
+	}
+
+	public static int BNP(int balance, int[] cost) {
 		
-		for (int i = 0; i < observation; i++) {
-			int n = in.nextInt();
-			int location = in.nextInt();
-						
-			if (mark[n-1] == false) {
-				mark[n-1] = true;
-				cows[n-1] = location;
+		int count = 0;
+		
+		for (int i = 0; i < 14; i++) {
+			if (balance/cost[i] > 0) {
+				int buying = balance/cost[i];
+				count = count + balance/cost[i];
+				balance = balance - buying*cost[i];
 			}
-			
-			else if (cows[n-1] != location) {
-				cnt++;
-				cows[n-1] = location;
-			}
-			
 		}
 		
-		System.out.println(cnt);
+		return balance+count*cost[13];
+	}
+
+	public static int Timing(int balance, int[] cost) {
+		int count = 0;
+		int upCount = 0;
+		int downCount = 0;
+		
+		
+		for (int i = 0; i < 14; i++) {
+			if (i != 0 && cost[i - 1] > cost[i]) {
+				downCount++;
+			} else if (i != 0 && cost[i - 1] < cost[i]) {
+				upCount++;
+			}
+
+			if (downCount >= 3 && balance / cost[i] > 0) {
+				int buying = balance / cost[i];
+				count = count + balance / cost[i];
+				balance = balance - buying * cost[i];
+			} else if (upCount >= 3 && count > 0) {
+				balance = balance + count * cost[i];
+				count = 0;
+			}
+
+			if (i != 13 && cost[i] <= cost[i + 1]) {
+				downCount = 0;
+			}
+
+			if (i != 13 && cost[i] >= cost[i + 1]) {
+				upCount = 0;
+			}
+		}
+		
+	return balance+count*cost[13];
+	
 	}
 }
