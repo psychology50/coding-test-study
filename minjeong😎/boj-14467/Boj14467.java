e3041f6150b4433e033de7f608b32d26169dbeca
byeon22 <minjeongs0202@gmail.com>
2023-09-15 15:43:52 +0900

    fix: 코드리뷰 후 boj-14467, 20546 수정

--- minjeong😎/boj-14467/Boj14467.java
+++ minjeong😎/boj-14467/Boj14467.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BOJ14467 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -14,8 +15,9 @@ public class BOJ14467 {
 		int cnt = 0;
 		
 		for (int i = 0; i < observation; i++) {
-			int n = Integer.parseInt(br.readLine());
-			int location = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int n = Integer.parseInt(st.nextToken());
+			int location = Integer.parseInt(st.nextToken());
 						
 			if (mark[n-1] == false) {
 				mark[n-1] = true;
