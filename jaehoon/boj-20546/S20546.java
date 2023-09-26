7b1d5ab1b377fbf59d07eaa761f3c84c4684b50a
dekoms <dekoms@yu.ac.kr>
2023-09-26 17:37:57 +0900

    fix: 20546 성공!

--- jaehoon/boj-20546/S20546.java
+++ jaehoon/boj-20546/S20546.java
@@ -2,137 +2,102 @@ package baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
-class Person {
-	private int money;
-	private int[] chart;
-	private int count;
+public class S20546 {
 
-	public Person(int money, int[] chart) {
-		this.money = money;
-		this.chart = chart;
-		this.count = 0;
-	}
+	public static int check(int[] chart, int day) {
+		int[] check = new int[3];
+		Arrays.fill(check, -1);
 
-	public int check(int[] check) {
-		int flag = 0;
-
-		for (int i = 0; i < check.length; i++) {
-			if (check[i] == 1)
-				flag++;
-			else if (check[i] == 0)
-				flag--;
+		int cnt = 0;
+		for (int i = day - 3; i < day; i++) {
+			if (chart[i] < chart[i + 1]) {
+				check[cnt] = 1;
+				cnt++;
+			} else if (chart[i] > chart[i + 1]) {
+				check[cnt] = 0;
+				cnt++;
+			} else {
+				cnt++;
+			}
 		}
 
-		if (flag == 3)
+		int flag = 0;
+		for (int i = 0; i < check.length; i++) {
+			if (check[i] == 1) {
+				flag++;
+			} else if (check[i] == 0) {
+				flag--;
+			} else
+				return -1;
+		}
+
+		if (flag == 3) // 전량 매도
 			return 1;
-		else if (flag == -3)
+		else if (flag == -3) // 전량 매수
 			return 0;
 
 		return -1;
 	}
 
-	public void BNP() {
-		int day = 1;
-		int mul = 1;
-		while (true) {
-			if (day == 15) {
-				break;
-			}
-			if (money < chart[day - 1]) {
-				day++;
-				continue;
-			}
-
-			while (!(money < chart[day - 1] * mul)) {
-				mul++;
-			}
-			mul--;
-			money = money - chart[day - 1] * mul;
-
-			count += mul;
-			mul = 1;
-
-			day++;
-		}
-	}
-
-	public void TIMING() {
-		int day = 1;
-		int mul = 1;
-		int[] check = new int[3];
-		while (true) {
-			if (day == 12) {
-				break;
-			}
-			if (day < 4) {
-				day++;
-				continue;
-			}
-			if (money < chart[day - 1]) {
-				day++;
-				continue;
-			}
-
-			for (int i = 0; i < check.length; i++) {
-				if (chart[(day - 1) + i] < chart[(day - 1) + i + 1]) {
-					check[i] = 1;
-				} else if (chart[(day - 1) + i] > chart[(day - 1) + i + 1]) {
-					check[i] = 0;
-				}
-
-			}
-
-			if (check(check) == 1) {
-				while (!(money < chart[day - 1] * mul)) {
-					mul++;
-				}
-				mul--;
-				money = money - chart[day - 1] * mul;
-
-				count += mul;
-				mul = 1;
-
-				day++;
-			} else if (check(check) == 0) {
-				money += chart[day - 1] * count;
-
-				count = 0;
-
-				day++;
-			} else {
-				day++;
-			}
-
-		}
-	}
-
-	public int getMoney() {
-		return money + chart[13] * count;
-	}
-
-}
-
-public class S20546 {
-
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int money = Integer.parseInt(br.readLine());
 		String[] input = br.readLine().split(" ");
-
 		int[] chart = new int[14];
 		for (int i = 0; i < chart.length; i++) {
 			chart[i] = Integer.parseInt(input[i]);
 		}
 
-		Person junhyeon = new Person(money, chart);
-		junhyeon.BNP();
-		Person sungmin = new Person(money, chart);
-		sungmin.TIMING();
+		int junMoney = money;
+		int junCount = 0;
 
-		if (junhyeon.getMoney() > sungmin.getMoney())
+		for (int day = 0; day < chart.length; day++) {
+			if (chart[day] > junMoney) {
+				continue;
+			}
+
+			int mul = 1;
+			while (junMoney >= chart[day] * mul) {
+				mul++;
+			}
+			junCount += --mul;
+			junMoney -= chart[day] * junCount;
+
+		}
+		junMoney += chart[13] * junCount;
+
+		int seoungMoney = money;
+		int seoungCount = 0;
+
+		// 4일치 데이터가 있어야 시작함.
+		for (int day = 3; day < chart.length; day++) {
+			if (chart[day] > seoungMoney) {
+				continue;
+			}
+
+			if (check(chart, day) == 0) {
+				int mul = 1;
+				while (seoungMoney >= chart[day] * mul) {
+					mul++;
+				}
+				seoungCount += --mul;
+				seoungMoney -= chart[day] * seoungCount;
+			} else if (check(chart, day) == 1) {
+				seoungMoney += chart[day] * seoungCount;
+				seoungCount = 0;
+			} else {
+				continue;
+			}
+
+		}
+		seoungMoney += chart[13] * seoungCount;
+
+		if (junMoney > seoungMoney)
 			System.out.println("BNP");
-		else if (junhyeon.getMoney() < sungmin.getMoney())
+		else if (junMoney < seoungMoney)
 			System.out.println("TIMING");
 		else
 			System.out.println("SAMESAME");
