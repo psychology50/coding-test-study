110d15165ad721c5326c5121cc5f9e71e3c90880
byeon22 <minjeongs0202@gmail.com>
2023-11-13 01:58:48 +0900

    feat: D1_2029

--- minjeong😎/SWEA/D1/D1_1545.java
+++ minjeong😎/SWEA/D1/D1_1545.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class D1_1945 {
+public class D1_1545 {
 
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
